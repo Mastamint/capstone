5dbfc14d6ed101493586e1464516a8e8d43749a1
Mastamint <Alnardo.barisic@gmail.com>
2022-02-15 15:53:01 -0800

    Search in Progress

--- Meal_Planner/MealPlanner/views.py
+++ Meal_Planner/MealPlanner/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'MealPlanner/index.html')
+    return render(request, 'MealPlanner/ingredients.html')
+
+def random(request):
+    return render(request, 'MealPlanner/randomRecipe.html')
