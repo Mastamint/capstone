c97ec26d4814c3f2e047ba53ed5d218846745e13
Mastamint <Alnardo.barisic@gmail.com>
2022-02-17 16:25:38 -0800

    Comments in view/MealPlanner

--- Meal_Planner/MealPlanner/views.py
+++ Meal_Planner/MealPlanner/views.py
@@ -6,3 +6,8 @@ def index(request):
 
 def random(request):
     return render(request, 'MealPlanner/randomRecipe.html')
+
+
+# Have to create a view/button to save the ID of the recipe to a list
+# Then have to call the list later and loop over it in a user profile
+# Still have to create user model possibly with a ManytoMany relationship
