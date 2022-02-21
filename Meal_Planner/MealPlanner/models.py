331fbea5c3dd77cd903121121739d4a93da4813f
Mastamint <Alnardo.barisic@gmail.com>
2022-02-20 18:48:12 -0800

    Working on saving recipes

--- Meal_Planner/MealPlanner/models.py
+++ Meal_Planner/MealPlanner/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from jsonfield import JSONField
 from django.contrib.auth.models import User
+from users.models import MyUser
 
 
-# Create your models here.
+class Recipe(models.Model):
+    recipe_id = models.IntegerField()
+    recipe_name = models.CharField()
+
+    user = models.ForeignKey(MyUser, on_delete=models.PROTECT, related_name="favorite_recipes")
+
+    def __str__(self):
+        return self.recipe_name
+
--- Meal_Planner/MealPlanner/views.py
+++ Meal_Planner/MealPlanner/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import json
+
+from .models import Recipe
+
 
 # Create your views here.
 def index(request):
@@ -7,6 +12,25 @@ def index(request):
 def random(request):
     return render(request, 'MealPlanner/randomRecipe.html')
 
+def get_todos(request):
+    data = {'info': []}
+    fav_recipes = Recipe.objects.all()
+    for recipe in fav_recipes:
+        recipe_dict = {
+            'id': recipe.recipe_id,
+            'name': recipe.recipe_name
+        }
+        data['info'].append(recipe_dict)
+    print(data)
+    return JsonResponse(data)
+
+def add_new_todo(request):
+    data = json.loads(request.body) # request.body is the data property of the axios call
+    # it comes in as a string, so json.loads take in that string and returns a python dictionary
+    # print(data) # {'text': 'test'}
+    new_id = data.get('id')
+    Recipe.objects.create(recipe_id=new_id)
+    return JsonResponse({'message': 'OK'})
 
 # Have to create a view/button to save the ID of the recipe to a list
 # Then have to call the list later and loop over it in a user profile
