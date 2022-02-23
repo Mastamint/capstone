ce967a4a06fd580fd54e6b9164a3096ca8d0e1ae
Mastamint <Alnardo.barisic@gmail.com>
2022-02-22 16:29:36 -0800

    WIP

--- Meal_Planner/MealPlanner/models.py
+++ Meal_Planner/MealPlanner/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from users.models import MyUser
-
+from jsonfield import JSONField
 
 class Recipe(models.Model):
     recipe_id = models.IntegerField()
     recipe_name = models.CharField(max_length=200)
+
+    recipe_info = JSONField(null=True)
+
 
     user = models.ForeignKey(MyUser, on_delete=models.PROTECT, related_name="favorite_recipes")
 
--- Meal_Planner/MealPlanner/views.py
+++ Meal_Planner/MealPlanner/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.http import JsonResponse
 import json
 
@@ -12,9 +12,11 @@ def index(request):
 def random(request):
     return render(request, 'MealPlanner/randomRecipe.html')
 
-def get_recipes(request):
+def get_recipes(request, id):
+    # if request.user.is_authenticated(): ... do stuff you would do if they were logged in
+    # request.user will give you the user
     data = {'info': []}
-    fav_recipes = Recipe.objects.all()
+    fav_recipes = get_object_or_404(Recipe, id=id, user=request.user)
     for recipe in fav_recipes:
         recipe_dict = {
             'id': recipe.recipe_id,
@@ -26,8 +28,12 @@ def get_recipes(request):
 
 def add_new_recipe(request):
     data = json.loads(request.body) 
-    new_id = data.get('id')
-    Recipe.objects.create(recipe_id=new_id)
+    print(data)
+    recipe_data = data.get('recipe')
+    # print(recipe_data)
+    new_id = recipe_data.get('id')
+    new_name = recipe_data.get('title')
+    Recipe.objects.create(recipe_id=new_id, recipe_name=new_name, user=request.user, recipe_info=recipe_data)
     return JsonResponse({'message': 'OK'})
 
 # Have to create a view/button to save the ID of the recipe to a list
